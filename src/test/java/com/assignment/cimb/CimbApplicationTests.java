5fc42895d6b05b53bcb774cebcbbc028db10ff98
peeranut.t <peeranut.t@entronica.in.th>
2025-02-24 15:56:26 +0700

    Initial project

---
+++ src/test/java/com/assignment/cimb/CimbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.cimb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CimbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
